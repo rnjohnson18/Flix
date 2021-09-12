df500ac41e40d01d0f229054d0e7ff985bc6e9a7
Ryan Johnson <rnjohnson18@users.noreply.github.com>
2021-09-12 18:12:31 -0500

    Initial commit for Unit 1 assignment

---
+++ Flixster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flixster
+//
+//  Created by Ryan Johnson on 9/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
